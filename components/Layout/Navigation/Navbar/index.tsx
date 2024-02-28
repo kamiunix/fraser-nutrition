3be01a5a84c7a90c869d5a28181718df3060c3ee
Sam Champagne <sam.c.tur@gmail.com>
2024-02-28 11:30:14 -0700

    changed about us to me

--- components/Layout/Navigation/Navbar/index.tsx
+++ components/Layout/Navigation/Navbar/index.tsx
@@ -39,7 +39,7 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
                                     <p
                                         className="text-lg"
                                     >
-                                        About Us
+                                        About Me
                                     </p>
                                 </Link>
                             </li>
