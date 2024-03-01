c99d6230d2f154122980206131e215ca571ee3fe
Sam Champagne <sam.c.tur@gmail.com>
2024-03-01 12:12:41 -0700

    fixed production deployement

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -2,7 +2,50 @@ import Layout from "../../components/Layout/Layout";
 import WideImage from "../../components/WideImage/WideImage";
 import Collapsible from "../../components/common/Collapsible";
 import {useEffect, useState} from "react";
-import faqs from './faq.mock'
+
+const faqs = [
+    {
+        key: 1,
+        question: "Are all sessions virtual?",
+        answer: "Yes, all sessions are virtual. I use a secure video platform to meet with clients."
+    },
+    {
+        key: 2,
+        question: "What is the difference between a dietitian and a nutritionist?",
+        answer: "Dietitians are regulated health professionals who have a degree in food and nutrition, have completed an internship, and have passed a national exam. Nutritionists are not regulated and do not require a degree in food and nutrition."
+    },
+    {
+        key: 3,
+        question: "What is the cost of a session?",
+        answer: "The cost of an initial session is $135.00 and $95.00 for follow-ups."
+    },
+    {
+        key: 4,
+        question: "Do you direct bill insurance?",
+        answer: "I do not direct bill insurance companies at this time, but I can provide you with a receipt to submit to your insurance company for reimbursement."
+    },
+    {
+        key: 5,
+        question: "How long is a session?",
+        answer: "The initial session is 60 minutes and follow-up sessions are 45 minutes."
+    },
+    {
+        key: 6,
+        question: "What is your cancellation policy?",
+        answer: "I require 24 hours notice for cancellations. If you cancel with less than 24 hours notice, you will be charged the full session fee."
+    },
+    {
+        key: 7,
+        question: "What is your availability?",
+        answer: "I am available for virtual appointments on Tuesday and Thursday evenings."
+    },
+    {
+        key: 8,
+        question: "What provinces are you licensed to practice in?",
+        answer: "I am licensed to practice in Alberta and British Colombia."
+    }
+]
+
 
 const Faq = () => {
 
